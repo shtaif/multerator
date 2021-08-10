60b2a2cbb4624c9c98660fd28d70520c431f76b5
Dor Shtaif <dorshtaif@gmail.com>
2021-08-10 15:49:20 +0300

    refactor(multerator-error-class): refactor into TypeScript (#15)

---
+++ src/utils/MulteratorError.ts
@@ -0,0 +1,16 @@
+class MulteratorError<
+  TInfo extends object | undefined = undefined
+> extends Error {
+  code: string;
+  info?: TInfo;
+
+  constructor(message: string, code: string, info?: TInfo) {
+    super(message);
+    this.code = code;
+    this.info = info;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default MulteratorError;
